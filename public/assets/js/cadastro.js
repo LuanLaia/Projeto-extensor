2c160abd151c489eb69384d0d7056254cd586f8f
italolacerdaa <italosouza7263@gmail.com>
2025-06-30 20:51:49 -0300

    Fiz comentários na minha parte

--- public/assets/js/cadastro.js
+++ public/assets/js/cadastro.js
@@ -1,14 +1,20 @@
 function showForm(type) {
-    document.getElementById('form-usuario').classList.remove('active');
-    document.getElementById('form-empresa').classList.remove('active');
-    document.getElementById('usuarioBtn').classList.remove('active');
-    document.getElementById('empresaBtn').classList.remove('active');
+    // Remove a classe "active" dos dois formulários
+    document.getElementById("form-usuario").classList.remove("active");
+    document.getElementById("form-empresa").classList.remove("active");
 
-    if (type === 'usuario') {
-        document.getElementById('form-usuario').classList.add('active');
-        document.getElementById('usuarioBtn').classList.add('active');
+    // Remove a classe "active" dos dois botões
+    document.getElementById("usuarioBtn").classList.remove("active");
+    document.getElementById("empresaBtn").classList.remove("active");
+
+    // Verifica se o tipo é "usuario"
+    if (type === "usuario") {
+        // Ativa o formulário e o botão do usuário
+        document.getElementById("form-usuario").classList.add("active");
+        document.getElementById("usuarioBtn").classList.add("active");
     } else {
-        document.getElementById('form-empresa').classList.add('active');
-        document.getElementById('empresaBtn').classList.add('active');
+        // Ativa o formulário e o botão da empresa
+        document.getElementById("form-empresa").classList.add("active");
+        document.getElementById("empresaBtn").classList.add("active");
     }
 }
